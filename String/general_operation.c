86f3ab25436fb8c56fe6d71b0be3030c638bac4b
Niaz <thatisniaz@gmail.com>
2023-06-16 01:06:17 -0700

    char frequency is added in String/

--- String/general_operation.c
+++ String/general_operation.c
@@ -4,6 +4,8 @@
 int length(char *);
 void to_upper(char *);
 int word_count(char *);
+void frequency(char *);
+void to_lower(char *);
 
 int main(void) {
     char str[50];
@@ -19,6 +21,9 @@ int main(void) {
 
     // count the words
     printf("Total words: %d\n", word_count(str));
+
+    // checking char freq
+    frequency(str);
 
     return 0;
 }
@@ -42,6 +47,17 @@ void to_upper(char * s) {
     }
 }
 
+void to_lower(char * s) {
+    int i = 0;
+    while (s[i] != '\0') {
+        if (s[i] >= 65 && s[i] <= 90) {
+            // change case only if the char is from a-z
+            s[i] = s[i] + 32;
+        }
+        i++;
+    }
+}
+
 int word_count(char * s) {
     // iterate through the string and check for space followed by a char (upper or lower case)
     int i = 0;
@@ -56,5 +72,21 @@ int word_count(char * s) {
     }
     return word_count;
 } 
+
+void frequency(char * s) {
+    int freq[26] = {0};
+    to_lower(s);
+    int i = 0;
+    while (s[i] != '\0') {
+        int index = s[i] - 97;
+        freq[index]++;
+        i++;
+    }
+    for (i = 0; i<26; i++) {
+        if (freq[i] > 1) {
+            printf("'%c' occurred: %d\n", i+97, freq[i]);
+        }
+    }
+}
 
 
