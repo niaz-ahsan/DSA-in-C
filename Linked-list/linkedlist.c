336749779b6bdff06427e1ec7641110985678cc9
Niaz <thatisniaz@gmail.com>
2023-06-19 00:25:38 -0700

    push() added in Linked-list/

--- Linked-list/linkedlist.c
+++ Linked-list/linkedlist.c
@@ -13,10 +13,13 @@ struct Node *tail = NULL;
 // func prototypes
 void generate_list(int * arr, int size);
 void display_list();
+void push(int);
 
 int main(void) {
     int arr[] = {3, 5, -1, 2, 1000, -99};
     generate_list(arr, 6);
+    push(420);
+    push(923);
     display_list();
     return 0;
 }
@@ -45,4 +48,17 @@ void display_list() {
         current = current->next;
     }
     printf("NULL\n");
+}
+
+void push(int data) {
+    if (head) {
+        struct Node * current = (struct Node *) malloc(sizeof(struct Node));
+        current->data = data;
+        current->next = NULL;
+        tail->next = current;
+        tail = current;
+        return;
+    }     
+    int arr[] = {data};
+    generate_list(arr, 1);
 }
