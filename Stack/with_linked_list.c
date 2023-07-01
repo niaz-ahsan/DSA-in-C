f76d976d07726d19d6950d475d2176a6cf7a09fd
Niaz <thatisniaz@gmail.com>
2023-07-01 06:10:47 -0700

    pop() added in Stack/ linked list

--- Stack/with_linked_list.c
+++ Stack/with_linked_list.c
@@ -7,25 +7,29 @@ struct Node {
 };
 
 struct Stack {
-    int size;
+    int len;
     struct Node * top; // the left most Node (head), we'll expand on the left side.
     struct Node * start; // the right most Node (tail)
 };
 
 void push(int, struct Stack *);
+int pop(struct Stack *);
 void init_stack(struct Stack *);
 void display(struct Stack *);
 void free_st(struct Stack *);
 
 int main(void) {
     struct Stack st;
-    printf("Provide size: \n");
-    scanf("%d", &st.size);
 
     init_stack(&st);
     push(5, &st);
     push(4, &st);
     push(3, &st);
+    display(&st);
+    pop(&st);
+    pop(&st);
+    display(&st);
+    pop(&st);
     display(&st);
 
     // at the end of operation
@@ -34,6 +38,7 @@ int main(void) {
 
 void init_stack(struct Stack * st) {
     st->top = NULL;
+    st->len = 0;
 }
 
 void push(int data, struct Stack * st) {
@@ -44,16 +49,31 @@ void push(int data, struct Stack * st) {
         node->next = NULL;
         st->start = node;
         st->top = node;
+        st->len++;
         return;
     }
     // stack not empty
     node->next = st->top;
     st->top = node;
+    st->len++;
+}
+
+int pop(struct Stack * st) {
+    if (st->top == NULL) {
+        printf("Stack empty!\n");
+        return -1;
+    }
+    struct Node * current = st->top;
+    st->top = st->top->next;
+    int ret = current->data;
+    free(current);
+    st->len--;
+    return ret;
 }
 
 void display(struct Stack * st) {
-    printf("=================Displaying Stack===============\n");
-    printf("Size: %d\n", st->size);
+    printf("=================Display Stack===============\n");
+    printf("Length: %d\n", st->len);
     struct Node * current = st->top;
     int i = 0;
     while (current != NULL) {
@@ -61,8 +81,10 @@ void display(struct Stack * st) {
         current = current->next;
         i++;
     }
-    printf("top poniter pointing at data: %d\n", st->top->data);
-    printf("start pointer pointing at data: %d\n", st->start->data);
+    if (st->len) {
+        printf("top poniter pointing at data: %d\n", st->top->data);
+        printf("start pointer pointing at data: %d\n", st->start->data);
+    }
     printf("================End Display=====================\n");
 }
 
