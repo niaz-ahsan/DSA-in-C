72da44ff0ec5c5d4b95240e1e85a44635fc9f0ed
Niaz <thatisniaz@gmail.com>
2023-07-14 15:56:56 -0700

    Queue related functions in practice code inside Tree/

--- Tree/practice_binary_tree.c
+++ Tree/practice_binary_tree.c
@@ -25,7 +25,7 @@ void init_stack(struct Stack *, int);
 void push(struct Stack *, struct Node *);
 struct Node * pop(struct Stack *);
 // =========== Queue related functions ===========
-void init_queue(struct Queue *);
+void init_queue(struct Queue *, int);
 void enqueue(struct Queue *, struct Node *);
 struct Node * dequeue(struct Queue *);
 
@@ -48,6 +48,10 @@ void push(struct Stack * st, struct Node * node) {
 }
 
 struct Node * pop(struct Stack * st) {
+    if (st->end < 0) {
+        // if empty return NULL
+        return NULL;
+    }
     struct Node * removed_node = st->data[st->end];
     st->end--;
     st->len--;
@@ -58,6 +62,27 @@ struct Node * pop(struct Stack * st) {
 
 // ################### Queue related functions BEGIN ##########################
 
+void init_queue(struct Queue * q, int size) {
+    q->push_stack->data = (struct Node **) malloc(size * sizeof(struct Node));
+    q->push_stack->end = -1;
+    q->push_stack->len = 0;
 
+    q->pop_stack->data = (struct Node **) malloc(size * sizeof(struct Node));
+    q->pop_stack->end = -1;
+    q->pop_stack->len = 0;
+}
 
+void enqueue(struct Queue * q, struct Node * node) {
+    // add it later
+}
+
+struct Node * dequeue(struct Queue * q) {
+    if (q->pop_stack->end < 0) {
+        return NULL;
+    }
+    struct Node * removed_node = q->pop_stack->data[q->pop_stack->end];
+    q->pop_stack->end--;
+    q->pop_stack->len--;
+    return removed_node;
+}
 // ################### Queue related functions END ##########################
