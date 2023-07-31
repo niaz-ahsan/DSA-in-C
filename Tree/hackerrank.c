d49f1b860b5e1821faf19a68473f0cf9e82c42a1
Niaz Ahsan <thatisniaz@gmail.com>
2023-07-30 21:37:13 -0700

    Hackerrank Tree postOrder, inOrder solution added

--- Tree/hackerrank.c
+++ Tree/hackerrank.c
@@ -14,10 +14,30 @@ struct node {
 
 void preOrder( struct node *root) {
     printf("%d ", root->data);
-    if (root->left) {
+    if (root->left) 
         preOrder(root->left);
-    }
-    if (root->right) {
+    if (root->right) 
         preOrder(root->right);
-    }
+}
+
+// Problem: Tree: Postorder Traversal
+// link: https://www.hackerrank.com/challenges/tree-postorder-traversal/copy-from/338516136
+
+void postOrder( struct node *root) {
+    if (root->left)
+        postOrder(root->left);
+    if (root->right)
+        postOrder(root->right);
+    printf("%d ", root->data);
+}
+
+// Problem: Tree: Inorder Traversal
+// link: https://www.hackerrank.com/challenges/tree-inorder-traversal/copy-from/338516356
+
+void inOrder( struct node *root) {
+    if (root->left)
+        inOrder(root->left);
+    printf("%d ", root->data);    
+    if (root->right)
+        inOrder(root->right);
 }
